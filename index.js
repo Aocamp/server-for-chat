1295958c98977a9dd3e8a434e3d1a6fb48af9b73
Camp <c4mpunvrst@gmail.com>
2019-03-04 15:41:17 +0600

    create rooms

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ app.get('/', (req, res) => {
     res.send('Chat Server is running on port 8888')
 });
 
-    var room;
-
 io.on('connection', (socket) => {
     console.log('user connected');
 socket.on('connection', function(userNickname) {
     socket.on('room', function (roomName) {
         socket.join(roomName);
-        console.log(userNickname +" : has joined the " + roomName + "room" );
+        console.log(userNickname +" : has joined the " + roomName + " room" );
     });
 });
 
@@ -29,10 +27,6 @@ socket.on('new message', (user, roomName, messageContent) => {
     io.sockets.in(roomName).emit('message', message)
 
 });
-
-socket.on('reconnect', () => {
-        socket.emit('room', room)
-    })
 
 socket.on('disconnect', function() {
 
