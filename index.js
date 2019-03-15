8a282e538f0e6697f400fc08260062992988b241
Camp <c4mpunvrst@gmail.com>
2019-03-15 10:49:59 +0600

    add queries

--- index.js
+++ index.js
@@ -3,42 +3,105 @@ const express = require('express'),
     app = express(),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
-app.get('/', (req, res) => {
 
+let mysql = require('mysql');
+
+let nameOfRoom;
+let idOfRoom;
+let idOfUser;
+
+let db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: '3306',
+    database: 'server_db'
+});
+
+db.connect(function(err){
+    if (err) console.log(err);
+    console.log("database connected")
+});
+
+app.get('/', (req, res) => {
     res.send('Chat Server is running on port 8888')
 });
 
 io.on('connection', (socket) => {
     console.log('user connected');
-socket.on('connection', function(userNickname) {
-    socket.on('room', function (roomName) {
-        socket.join(roomName);
-        console.log(userNickname +" : has joined the " + roomName + " room" );
+
+    socket.on('connection', function(userNickname) {
+        socket.on('room', function (roomName) {
+            db.query('SELECT id FROM users WHERE user_login = ?', userNickname,function(err,rows){
+                if(err)
+                    return console.log(err);
+                if (!rows.length)
+                {
+                    db.query('INSERT INTO users (user_login) VALUES (?)', userNickname);
+                }
+                else
+                {
+                    console.log("user exist");
+                    let s = JSON.stringify(rows[0].id);
+                    let data = JSON.parse(s);
+                    idOfUser = data;
+
+                    db.query('SELECT id FROM rooms WHERE user_id = ?', data, function (err,roomId) {
+                        if(err)
+                        {
+                            return console.log(err);
+                        }
+                        if(!roomId.length)
+                        {
+                            socket.join(roomName);
+                            let params = [roomName, data];
+                            db.query('INSERT INTO rooms (room_name, user_id) VALUES (?, ?)', params);
+                        }
+                        else {
+                            console.log("room exist");
+                            let a = JSON.stringify(roomId[0].id);
+                            let roomData = JSON.parse(a);
+                            idOfRoom = roomData;
+
+                            db.query('SELECT room_name FROM rooms where id = ?', roomData, function (err, room) {
+                                if(err)
+                                    return console.log(err);
+                                else {
+                                    let name = JSON.stringify(room[0].room_name);
+                                    let userRoom = JSON.parse(name);
+                                    socket.join(userRoom);
+                                    console.log(userNickname +" : has joined the " + userRoom + " room" );
+                                    //console.log(io.sockets.sockets[userRoom]);
+                                    nameOfRoom = userRoom;
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        });
     });
-});
 
-socket.on('new message', (user, roomName, messageContent) => {
-    //log the message in console
-    console.log(user + ": "+ messageContent);
+    socket.on('new message', (user, messageContent, messageDate) => {
+        console.log(user + ": "+ messageContent);
 
-    //create a message object
-    let  message = {"message":messageContent, "user":user};
+        let params = [messageContent, messageDate, idOfRoom, idOfUser];
 
-    io.sockets.in(roomName).emit('message', message)
+        db.query('INSERT INTO messages (message_text, message_date, room_id, user_id) VALUES(?, ?, ?, ?)', params);
 
-});
+        let  message = {"message":messageContent, "user":user};
 
-socket.on('disconnect', function() {
+        io.sockets.in(nameOfRoom).emit('message', message)
+    });
 
-    console.log('User has left ');
+    socket.on('disconnect', function() {
 
-    socket.broadcast.emit( "user disconnect" ,' user has left')
+        console.log('User has left ');
 
-})
-
+        socket.broadcast.emit( "user disconnect" ,' user has left')
+    })
 });
 
 server.listen(8888,()=>{
-
     console.log('Node app is running on port 8888')
 });
